6481666cbe3e61ac09bf8eaadf50b070cf2b63af
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-03-11 21:40:05 +0800

    feat: add gap in content wrapper's flexbo

--- src/components/content-wrapper/content-wrapper.tsx
+++ src/components/content-wrapper/content-wrapper.tsx
@@ -5,11 +5,11 @@ import BadgeFilter from "../navbar/badge-filter";
 const ContentWrapper = ({ children }: TChildren) => {
   return (
     <div className="h-full w-full pt-6">
-      <div className="page-container py-2 mt-12 flex">
+      <div className="page-container py-2 mt-12 flex gap-7">
         <div className="flex-1">
           <Sidebar />
         </div>
-        <div className="flex-[7]">
+        <div className="flex-[8]">
           <BadgeFilter />
           <div className="w-full h-full">{children}</div>
         </div>
