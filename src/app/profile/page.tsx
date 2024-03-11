0d0ebc7e5412e22bb6abec059eee7f4a4088273d
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-03-11 21:30:13 +0800

    feat: instantiate context file

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,7 +2,7 @@ import { ContentWrapper } from "@/components/content-wrapper";
 import React from "react";
 
 const Profile = () => {
-  return <ContentWrapper>Profile</ContentWrapper>;
+  return <div>Profile Page</div>;
 };
 
 export default Profile;
---
+++ src/context/nav-context.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useState } from "react";
+import { createContext } from "vm";
+
+type NavProviderProps = {
+  children: React.ReactNode;
+};
+
+const NavContext = createContext();
+
+export const NavProvider = ({ children }: NavProviderProps) => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+  const toggleNavbarVisibility = () => {
+    setIsNavExpanded(!isNavExpanded);
+  };
+
+  return (
+    <NavContext.Provider value={{ isNavExpanded, toggleNavbarVisibility }}>
+      {children}
+    </NavContext.Provider>
+  );
+};
+
+export default NavContext;
