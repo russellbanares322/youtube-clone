f9369a1f82d59788bc659ff43433d62fc0db8870
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-03-11 20:27:46 +0800

    feat: add flex property in sidebar and content

--- src/components/content-wrapper/content-wrapper.tsx
+++ src/components/content-wrapper/content-wrapper.tsx
@@ -5,11 +5,11 @@ import BadgeFilter from "../navbar/badge-filter";
 const ContentWrapper = ({ children }: TChildren) => {
   return (
     <div className="h-full w-full pt-6">
-      <div className="flex page-container py-2 mt-12">
-        <div>
+      <div className="page-container py-2 mt-12 flex">
+        <div className="flex-1">
           <Sidebar />
         </div>
-        <div>
+        <div className="flex-[7]">
           <BadgeFilter />
           <div className="w-full h-full">{children}</div>
         </div>
