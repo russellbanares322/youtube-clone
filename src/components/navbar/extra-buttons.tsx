2c03a75ed811cc94ca38e26acd268a62ab9bfda7
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-03-28 09:55:24 +0800

    style: change menu icon's size in navbar's extra button

--- src/components/navbar/extra-buttons.tsx
+++ src/components/navbar/extra-buttons.tsx
@@ -25,7 +25,7 @@ const ExtraButtons = () => {
       <UserButton />
       <SignedOut>
         <div className="flex items-center justify-center gap-4">
-          <CgMoreVerticalAlt size={20} />
+          <CgMoreVerticalAlt size={21} />
           <SignInButton>
             <button className="flex items-center gap-2 border border-zinc-500 text-blue-400 py-1 px-3 rounded-full hover:bg-blue-400/45 hover:border-blue-400/45">
               <LuUserCircle2 />
