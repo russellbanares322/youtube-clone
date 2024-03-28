9d9537d8895127e5042850147dc8f7087f650d56
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-03-28 20:34:18 +0800

    feat: implement badge filter functionality

--- src/components/navbar/badge-filter.tsx
+++ src/components/navbar/badge-filter.tsx
@@ -1,14 +1,49 @@
+"use client";
+
 import { filterOptionsData } from "@/data/filter-options-data";
+import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 const BadgeFilter = () => {
+  const [selectedFilterOption, setSelectedFilterOption] = useState<string[]>([
+    "Everything",
+  ]);
+
+  const isOptionSelected = (filterOption: string) => {
+    const isFilterOptionSelected = selectedFilterOption.includes(filterOption);
+
+    if (isFilterOptionSelected) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const handleSelectFilterOption = (filterOption: string) => {
+    if (isOptionSelected(filterOption)) {
+      const filteredSelectedFilterOption = selectedFilterOption.filter(
+        (option) => option !== filterOption
+      );
+      setSelectedFilterOption(filteredSelectedFilterOption);
+    } else {
+      setSelectedFilterOption([...selectedFilterOption, filterOption]);
+    }
+  };
+
   return (
     <div className="flex items-center gap-2 w-full mb-5">
-      {filterOptionsData.map((item) => (
+      {filterOptionsData.map((filterOption) => (
         <p
-          key={item}
-          className="text-sm bg-gray p-2 w-max rounded-md font-semibold cursor-pointer"
+          onClick={() => handleSelectFilterOption(filterOption)}
+          key={filterOption}
+          className={twMerge(
+            isOptionSelected(filterOption)
+              ? "bg-white text-black"
+              : "bg-gray text-white",
+            "text-sm py-2 px-3 w-max rounded-md font-semibold cursor-pointer"
+          )}
         >
-          {item}
+          {filterOption}
         </p>
       ))}
     </div>
