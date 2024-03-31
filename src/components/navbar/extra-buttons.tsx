6aa53e172bdfc7518ebfc4ff05bda8761aa61637
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-03-31 20:02:07 +0800

    feat: add path property in data items

--- src/components/navbar/extra-buttons.tsx
+++ src/components/navbar/extra-buttons.tsx
@@ -27,7 +27,10 @@ const ExtraButtons = () => {
       {!isSignedIn && (
         <div className="flex items-center justify-center gap-4">
           <Tooltip title="Settings">
-            <CgMoreVerticalAlt className="cursor-pointer" size={20} />
+            <CgMoreVerticalAlt
+              className="cursor-pointer icon-hover p-2"
+              size={40}
+            />
           </Tooltip>
           <SignInButton>
             <button className="flex items-center gap-2 border border-zinc-500 text-blue-400 py-1 px-3 rounded-full hover:bg-blue-400/45 hover:border-blue-400/45">
--- src/data/sidebar-items.tsx
+++ src/data/sidebar-items.tsx
@@ -16,14 +16,17 @@ export const firstItems = [
   {
     title: "Home",
     icon: <GoHomeFill size={iconSize} />,
+    path: "/",
   },
   {
     title: "Shorts",
     icon: <SiYoutubeshorts size={iconSize} />,
+    path: "/shorts",
   },
   {
     title: "Subscriptions",
     icon: <MdOutlineSubscriptions size={iconSize} />,
+    path: "/subscriptions",
   },
 ];
 
@@ -32,26 +35,31 @@ export const secondItems = [
     title: "Your channel",
     needsAuthentication: false,
     icon: <PiUserSquare size={23} />,
+    path: "/my-channel",
   },
   {
     title: "History",
     needsAuthentication: false,
     icon: <GrHistory size={iconSize} />,
+    path: "/history",
   },
   {
     title: "Playlists",
     needsAuthentication: true,
     icon: <RiPlayList2Fill size={iconSize} />,
+    path: "/playlists",
   },
   {
     title: "Your videos",
     needsAuthentication: true,
     icon: <BiPlay className="border border-solid" size={iconSize} />,
+    path: "/my-videos",
   },
   {
     title: "Watch later",
     needsAuthentication: true,
     icon: <LuClock4 size={iconSize} />,
+    path: "/watch-later",
   },
 ];
 
@@ -59,29 +67,36 @@ export const fourthItems = [
   {
     title: "Trending",
     icon: <BsFire size={iconSize} />,
+    path: "/trending",
   },
   {
     title: "Music",
     icon: <PiMusicNote size={iconSize} />,
+    path: "/music",
   },
   {
     title: "Movies",
     icon: <GiClapperboard size={iconSize} />,
+    path: "/movies",
   },
   {
     title: "Gaming",
     icon: <GrGamepad size={iconSize} />,
+    path: "/gaming",
   },
   {
     title: "news",
     icon: <PiNewspaper size={iconSize} />,
+    path: "/news",
   },
   {
     title: "Sports",
     icon: <IoTrophyOutline size={iconSize} />,
+    path: "/sports",
   },
   {
     title: "Fashion and Beauty",
     icon: <GiHanger size={iconSize} />,
+    path: "/fashion",
   },
 ];
