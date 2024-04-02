d4ce784c3da3bf58fa3aad9a8132234b088e5b17
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-04-02 20:22:19 +0800

    feat: set only sidebar item to active if the sidebar is expanded

--- src/components/sidebar/fourth-items-display.tsx
+++ src/components/sidebar/fourth-items-display.tsx
@@ -1,14 +1,18 @@
+import { useNavContext } from "@/context/nav-context";
 import { fourthItems } from "@/data/sidebar-items";
 import { isSidebarItemActive } from "@/utils/isSidebarItemActive";
 import { twMerge } from "tailwind-merge";
 
 const FourthItemsDisplay = () => {
+  const { isNavExpanded } = useNavContext();
   return (
     <div className="flex flex-col text-[0.93rem]">
       {fourthItems.map((item) => (
         <p
           className={twMerge(
-            isSidebarItemActive(item.pathName) ? "active-sidebar-item" : "",
+            isNavExpanded && isSidebarItemActive(item.pathName)
+              ? "active-sidebar-item"
+              : "",
             "sidebar-item gap-6"
           )}
           key={item.title}
