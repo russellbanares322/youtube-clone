6b42e260bc7371c70db646015727fa6166be6836
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-04-03 20:10:20 +0800

    feat: add grid css property in content wrapper

--- src/components/content-wrapper/content-wrapper.tsx
+++ src/components/content-wrapper/content-wrapper.tsx
@@ -8,11 +8,11 @@ const ContentWrapper = ({ children }: TChildren) => {
     <div className="relative">
       <Navbar />
       <div className="h-full w-full pt-6">
-        <div className="page-container py-2 mt-12 flex gap-7">
+        <div className="page-container py-2 mt-12 grid grid-cols-7 gap-10">
           <div>
             <Sidebar />
           </div>
-          <div className="w-full">
+          <div className="w-full col-span-6">
             <BadgeFilter />
             <div className="w-full h-full">{children}</div>
           </div>
