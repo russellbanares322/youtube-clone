92eed57487f5fd25e49dc7e8dc8a40a1c102278d
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-04-12 21:00:21 +0800

    feat: wrap layout component with ConvexClientProvider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { ContentWrapper } from "@/components/content-wrapper";
 import "./globals.css";
 import Providers from "./providers";
 import { ClerkProvider } from "@clerk/nextjs";
+import ConvexClientProvider from "./ConvexClientProvider";
 
 export const metadata = {
   title: "Youtube Clone",
@@ -10,22 +11,24 @@ export const metadata = {
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ClerkProvider
-      afterSignInUrl="/"
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
-      <html>
-        <head>
-          <title>{metadata?.title}</title>
-          <meta name="description" content={metadata.description} />
-        </head>
-        <body>
-          <Providers>
-            <ContentWrapper>{children}</ContentWrapper>
-          </Providers>
-        </body>
-      </html>
-    </ClerkProvider>
+    <ConvexClientProvider>
+      <ClerkProvider
+        afterSignInUrl="/"
+        publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+      >
+        <html>
+          <head>
+            <title>{metadata?.title}</title>
+            <meta name="description" content={metadata.description} />
+          </head>
+          <body>
+            <Providers>
+              <ContentWrapper>{children}</ContentWrapper>
+            </Providers>
+          </body>
+        </html>
+      </ClerkProvider>
+    </ConvexClientProvider>
   );
 };
 
