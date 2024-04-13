5ce64510f1e7031c6589243901776f8894d952a3
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-04-13 20:25:48 +0800

    feat: modify layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,27 @@
 import { ContentWrapper } from "@/components/content-wrapper";
 import "./globals.css";
 import Providers from "./providers";
-import { ClerkProvider } from "@clerk/nextjs";
-import ConvexClientProvider from "./ConvexClientProvider";
+import { ClerkProvider, useAuth } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
 
 export const metadata = {
   title: "Youtube Clone",
   description: "Simple web application that mimics youtube.",
 };
 
+const { NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY, NEXT_PUBLIC_CONVEX_URL } =
+  process.env;
+
+const convex = new ConvexReactClient(NEXT_PUBLIC_CONVEX_URL as string);
+
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ConvexClientProvider>
-      <ClerkProvider
-        afterSignInUrl="/"
-        publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-      >
+    <ClerkProvider
+      afterSignInUrl="/"
+      publishableKey={NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         <html>
           <head>
             <title>{metadata?.title}</title>
@@ -27,8 +33,8 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
             </Providers>
           </body>
         </html>
-      </ClerkProvider>
-    </ConvexClientProvider>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
   );
 };
 
