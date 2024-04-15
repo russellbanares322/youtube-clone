9382cd554fed0f80e410d6acd8b3aa2c24c396db
russell-banares <russell.banares@abedatasolutions.com.ph>
2024-04-15 20:52:17 +0800

    fix: resolve convex provider not being recognized

--- src/app/ConvexClientProvider.tsx
+++ src/app/ConvexClientProvider.tsx
@@ -1,10 +1,25 @@
 "use client";
-import { ConvexProvider, ConvexReactClient } from "convex/react";
 
-const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+import { useAuth } from "@clerk/nextjs";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+
+const convex = new ConvexReactClient(
+  process.env.NEXT_PUBLIC_CONVEX_URL as string
+);
 
 const ConvexClientProvider = ({ children }: { children: React.ReactNode }) => {
-  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
+  return (
+    <ClerkProvider
+      afterSignInUrl="/"
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
 };
 
 export default ConvexClientProvider;
